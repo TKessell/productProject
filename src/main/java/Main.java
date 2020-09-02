b32511e0c2ce04fb86cbb45cc43e83e77ff2cee1
TKessell <domenic.t.kessell@gmail.com>
2020-09-02 18:42:22 -0400

    change location of stylesheet.css

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
