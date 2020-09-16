69c2ccce6a7d75ee39c834da6ba8a1c3d5dd1150
TKessell <domenic.t.kessell@gmail.com>
2020-09-16 17:17:12 -0400

    update

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,3 +1,8 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -7,14 +12,12 @@ import javafx.scene.control.Label;
 public class Controller {
 
   @FXML
-  private void fill() {
-  }
-
   public void addProduct(ActionEvent actionEvent) {
     System.out.println("Acknowledged");
   }
-
   public void recordRecord(ActionEvent actionEvent) {
     System.out.println("Recorded");
   }
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
 
   public static void main(String[] args) {
